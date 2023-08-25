529e2dfa0e57866e7869b7f9e0da644083e07c6b
Viktor <victor.sudarckin@yandex.ru>
2023-08-25 19:54:26 +0300

    Добавляет контроллеры и роуты авторизации и аутентификации

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const NotFoundError = require('../errors/NotFoundError');
 const BedRequestError = require('../errors/BedRequestError');
@@ -34,7 +36,49 @@ const updateUser = (req, res, next) => {
     });
 };
 
+const createUser = (req, res, next) => {
+  const { password } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        ...req.body,
+        password: hash,
+      })
+        .then((user) => {
+          const {
+            _id, name, email,
+          } = user;
+          res.status(201).send({
+            _id, name, email,
+          });
+        })
+        .catch((err) => {
+          if (err.name === 'ValidationError') {
+            next(new BedRequestError('Переданы некорректные данные при создании пользователя'));
+          } else if (err.code === 11000) {
+            next(new ConflictError('Такой email уже существует на сервере.'));
+          } else {
+            next(err);
+          }
+        });
+    });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, 'super-secret', { expiresIn: '7d' });
+      res.send({ token });
+    })
+    .catch(next);
+};
+
 module.exports = {
   getUser,
   updateUser,
+  createUser,
+  login,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
 const NotFoundError = require('../errors/NotFoundError');
+const auth = require('../middlewares/auth');
+
+const {
+  createUser,
+  login,
+} = require('../controllers/users');
+
+router.use('/signin', login);
+router.use('/signup', createUser);
+
+router.use(auth);
 
 router.use('/movies', require('./movies'));
 router.use('/users', require('./users'));
